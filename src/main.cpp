7c2365113b28851c5b429bed19ec8cdc8687e87e
CGHSRobotics <cghsrobotics@gmail.com>
2022-11-18 13:19:22 -0500

    hi

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,37 @@
 using namespace vex;
 
 // A global instance of vex::brain used for printing to the V5 brain screen
-vex::brain       Brain;
+vex::brain Brain;
 
 // define your global instances of motors and other devices here
+gyro Gyro = gyro(Brain.ThreeWirePort.B);
 
+int main()
+{
 
-int main() {
+    Brain.Screen.printAt(10, 50, "Hello V5");
 
-    Brain.Screen.printAt( 10, 50, "Hello V5" );
-   
-    while(1) {
-        
+    Brain.Screen.clearLine(3, 1);
+    Brain.Screen.setCursor(3, 1);
+    Brain.Screen.print("STARTING CALIBRATION... ");
+
+    Gyro.startCalibration();
+    while (Gyro.isCalibrating())
+    {
+        wait(20, msec);
+    }
+
+    Brain.Screen.clearLine(3, 1);
+    Brain.Screen.setCursor(3, 1);
+    Brain.Screen.print("STARTING CALIBRATION... DONE");
+
+    while (1)
+    {
+
+        Brain.Screen.clearLine(3, 1);
+        Brain.Screen.setCursor(3, 1);
+        Brain.Screen.print(Gyro.rotation());
+
         // Allow other tasks to run
         this_thread::sleep_for(10);
     }
